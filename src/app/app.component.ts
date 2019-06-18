2319b87627693bdcd3f58235a4716902dac571b1
Tucker <tcozzens91@gmail.com>
2019-06-17 22:25:01 -0400

    Clean-up signalr working.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'primeng/api';
 
 import * as signalR from '@aspnet/signalr';
+import { connect } from 'tls';
 
 @Component({
   selector: 'app-root',
@@ -16,16 +17,14 @@ export class AppComponent implements OnInit {
   ngOnInit(): void {
     const connection = new signalR.HubConnectionBuilder()
       .configureLogging(signalR.LogLevel.Information)
-      .withUrl('http://signalrapi-prod.us-east-1.elasticbeanstalk.com/notify')
+      .withUrl('http://localhost:5000/notify')
+      // For prod
+      // .withUrl('http://signalrapi-prod.us-east-1.elasticbeanstalk.com/notify')
       .build();
 
-    // tslint:disable-next-line: only-arrow-functions
-    connection.start().then(function () {
-      console.log('Connected!');
-      // tslint:disable-next-line: only-arrow-functions
-    }).catch(function (err) {
-      return console.error(err.toString());
-    });
+    connection.start()
+      .then(() => console.log('Connected!'))
+      .catch((err) => console.log(err.toString()));
 
     connection.on('BroadcastMessage', (type: string, payload: string) => {
       this.messageService.add({ severity: type, summary: payload, detail: 'Via SignalR' });
