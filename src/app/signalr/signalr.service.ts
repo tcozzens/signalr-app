57aee1c86030b9c1e39318abb168d72e506e4f84
Tucker <tcozzens91@gmail.com>
2019-07-11 10:30:44 -0600

    signalr - setting environment variables

--- src/app/signalr/signalr.service.ts
+++ src/app/signalr/signalr.service.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import * as signalR from '@aspnet/signalr';
 
+import { environment } from '../../environments/environment';
+
 @Injectable()
 export class SignalRService {
   constructor(private httpClient: HttpClient, ) { }
@@ -11,10 +13,7 @@ export class SignalRService {
   buildSignalRConnection() {
     return new signalR.HubConnectionBuilder()
       .configureLogging(signalR.LogLevel.Information)
-      // For prod
-      // .withUrl('http://signalrapi-prod.us-east-1.elasticbeanstalk.com/notify')
-      // For dev
-      .withUrl('http://localhost:5000/notify')
+      .withUrl(environment.notifyUrl)
       .build();
   }
 
@@ -27,10 +26,7 @@ export class SignalRService {
   }
 
   submitNewMessage(username, message): Observable<any> {
-    // For prod
-    // return this.httpClient.post('http://signalrapi-prod.us-east-1.elasticbeanstalk.com/api/message',
-    // For dev
-    return this.httpClient.post('http://localhost:5000/api/message',
+    return this.httpClient.post(environment.messageUrl,
       {
         Type: 'message',
         Payload: JSON.stringify({ username, message, timestamp: new Date().toISOString() })
