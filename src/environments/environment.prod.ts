42461aa7c3d4b6e71b3f39ad82de2f2fab7c45e0
Tucker <tcozzens91@gmail.com>
2019-07-11 11:21:04 -0600

    signalr - fix link for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   notifyUrl: 'http://signalrapi-prod.us-east-1.elasticbeanstalk.com/notify',
-  messageUrl: 'https://5lqbx47uxa.execute-api.us-east-1.amazonaws.com/dev/messages'
+  messageUrl: 'http://signalrapi-prod.us-east-1.elasticbeanstalk.com/api/message'
 };
